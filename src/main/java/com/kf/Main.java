6e344a265d8eaa39bd39bb8ff343c3ccb2b07542
Kaya-F <kfisher@appdev.yearup.org>
2023-04-21 13:03:51 -0400

    added book class / properties and constructors

--- src/main/java/com/kf/Main.java
+++ src/main/java/com/kf/Main.java
@@ -2,6 +2,30 @@ package com.kf;
 
 public class Main {
     public static void main(String[] args) {
+        //{"The Seamstress", "Cinder", "Invisible Man", "Fahrenheit 451", "Pride & Prejudice"};
+        Book TheSeamstress = new Book ();
+        //fiveBooksNeeded
 
     }
+}
+
+// Create a book class
+class Book {
+    //properties of the books
+    public int bookId;
+    public String isbn;
+    public String bookTitle;
+    public Boolean isCheckedOut;
+    public String checkedOutTo;
+
+    //Constructor(s)
+    public Book(int bookId, String isbn, String bookTitle, Boolean isCheckedOut, String checkedOutTo){
+        this.bookId = bookId;
+        this.isbn = isbn;
+        this.bookTitle = bookTitle;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+}
+
+
 }
